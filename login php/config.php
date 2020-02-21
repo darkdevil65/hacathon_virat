617fb08d5c7ef65ce2b83f5538637c0171834581
darkdevil65 <viratvamja1234@gmail.com>
2020-02-21 10:01:46 +0530

    Initial commit

---
+++ login php/config.php
@@ -0,0 +1,9 @@
+<?php
+$db= mysqli_connect("localhost:3307","root","","hackthon");
+if(mysqli_error($db)) {
+	echo "failed";
+}
+else{
+	echo "";
+}
+  ?>
---
+++ login php/welcome.php
@@ -0,0 +1,25 @@
+<?php
+   include('session.php');
+?>
+<html">
+   
+   <head>
+      <title>Welcome </title>
+   </head>
+   
+   <body>
+   	<style type="text/css">
+   		a:hover{
+   			color: red;
+   		}
+   		a{
+   			font-size: 40px;
+   			border: 2px dashed red;
+   		}
+   	</style>
+   	  <h1><a href = "logout.php">Sign Out</a></h1>
+      <h1>Welcome </h1> 
+      
+   </body>
+   
+</html>
